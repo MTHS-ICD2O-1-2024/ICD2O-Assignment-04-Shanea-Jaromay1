80a112397a5866ae32cfefddf9d34158b5fbfcd5
Shanea-Jaromay1 <shanea.jaromay@stu.ocsb.ca>
2025-05-06 17:29:39 +0000

    init

--- js/script.js
+++ js/script.js
@@ -1,5 +1,30 @@
-// Copyright (c) 2020 Mr. Coxall All rights reserved
+// Copyright (c) 2025 Shanea Jaromay All rights reserved
 //
-// Created by: Mr. Coxall
-// Created on: Sep 2020
+// Created by: Shanea Jaromay
+// Created on: May 2025
 // This file contains the JS functions for index.html
+
+"use strict"
+
+/**
+ * This function generates random positive or negative number
+ */
+// eslint-disable-next-line no-unused-vars
+
+function generateNumber() {
+  // input
+  const optionPositive = document.getElementById("option-positive").checked
+
+  // generate a new random number every time
+  const randomNumber = Math.floor(Math.random() * 6) + 1
+
+  // output
+  if (optionPositive === true) {
+    document.getElementById("answer").innerHTML =
+      "Your random number is: " + randomNumber
+  } else {
+    const randomNegativeNumber = randomNumber * -1
+    document.getElementById("answer").innerHTML =
+      "Your random number is: " + randomNegativeNumber
+  }
+}
