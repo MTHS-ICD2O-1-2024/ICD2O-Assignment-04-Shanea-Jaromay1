6913d58893a44e8b5bf82331aa98dc4b8f13f667
Shanea-Jaromay1 <shanea.jaromay@stu.ocsb.ca>
2025-05-08 16:36:50 +0000

    pls work

--- js/script.js
+++ js/script.js
@@ -4,28 +4,50 @@
 // Created on: May 2025
 // This file contains the JS functions for index.html
 
+
 "use strict"
 
+
 /**
- * This function generates random positive or negative number
- */
+* This function generates random positive or negative number
+*/
 // eslint-disable-next-line no-unused-vars
 
 function calculatePrice() {
-  // input
-  const optionSingle = document.getElementById("option-single-bag").checked
-  const userCandy = document.getElementById()
+  const TAX_RATE = 0.13
+  let subTotal = 0
+  let bagType = ""
 
-  // generate a new random number every time
-  const randomNumber = Math.floor(Math.random() * 6) + 1
+  // Input
+  const optionSingle = document.getElementById('option-single-bag').checked
+  const optionFamily = document.getElementById('option-family-bag').checked
+  const userCandy = document.getElementById('user-candy').value
 
-  // output
-  if (optionPositive === true) {
-    document.getElementById("answer").innerHTML =
-      "Your random number is: " + randomNumber
+  // Process
+  if (optionSingle) {
+    subTotal = 5
+    bagType = "Single-size bag"
+  } else if (optionFamily) {
+    subTotal = 15
+    bagType = "Family-size bag"
   } else {
-    const randomNegativeNumber = randomNumber * -1
-    document.getElementById("answer").innerHTML =
-      "Your random number is: " + randomNegativeNumber
+    document.getElementById("answer").innerHTML = "Please select a bag size."
+    return
   }
+
+  // Tax calculation
+  const tax = subTotal * TAX_RATE
+  const totalPrice = subTotal + tax
+
+  // Output
+  document.getElementById("answer").innerHTML = 
+    'You selected ' + userCandy + ' in a ' + bagType + '.' <br>
+  'Sub Total: $' +
+  subTotal.toFixed(2) +
+  '<br>' +
+  'Tax: $' +
+  tax.toFixed(2) +
+  '<br>' +
+  'Total Price: $' +
+  totalPrice.toFixed(2)
 }
